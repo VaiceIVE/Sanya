5632c2ec8c0fb33114e92082796d9ccddc016305
Isachenko Ivan <vanisachenko@gmail.com>
2023-12-29 01:07:52 +0600

    fix: cors...

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors({
   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
-  origin: ['http://localhost:5173/', 'http://localhost:5173'],
+  origin: ['http://localhost:5173/', 'http://localhost:5173', 'http://178.170.192.87:5173/', 'http://178.170.192.87:5173'],
   optionsSuccessStatus: 200,
   credentials: true,
 }));
